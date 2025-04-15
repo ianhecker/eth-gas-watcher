518000489caf551aa1cac10f88442eaefb67677f
ian hecker <ianhecker.dev@gmail.com>
2025-04-15 15:43:04 -0600

    Chore[payload] Add Payload struct

---
+++ internal/endpoint/payload/payload.go
@@ -0,0 +1,30 @@
+package payload
+
+import (
+	"encoding/json"
+
+	"github.com/ianhecker/eth-gas-watcher/internal/desist"
+)
+
+type Payload map[string]interface{}
+
+func (p *Payload) MarshalJSON() ([]byte, error) {
+	bytes, err := json.Marshal(p)
+	if err != nil {
+		return nil, desist.Error("could not marshal payload", err)
+	}
+	return bytes, nil
+}
+
+func MakePayload() Payload {
+	return map[string]interface{}{
+		"jsonrpc": "2.0",
+		"id":      1,
+		"method":  "eth_feeHistory",
+		"params": []interface{}{
+			"0x5",             // Block count (hex encoded)
+			"latest",          // Newest block specifier
+			[]int{10, 50, 90}, // Reward percentiles
+		},
+	}
+}
